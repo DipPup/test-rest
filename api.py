b029e9eaef9b43d5ee2d24b57ad7350f7a0372b0
DipPup <100786590+DipPup@users.noreply.github.com>
2022-06-02 20:26:07 +0700

    Update api.py

--- api.py
+++ api.py
@@ -80,10 +80,10 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/')
 
-if __name__ == '__main__':
-    port = 5000 + random.randint(0, 999)
-    print(port)
-    url = "http://127.0.0.1:{0}".format(port)
-    print(url)
-    app.run(port=port, host='0.0.0.0')
+
+port = 5000 + random.randint(0, 999)
+print(port)
+url = "http://127.0.0.1:{0}".format(port)
+print(url)
+app.run(port=port, host='0.0.0.0')
 
